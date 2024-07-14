51c846a639a103293ddba4e4d79b250c58aea30b
MidnightCrowing <110297461+MidnightCrowing@users.noreply.github.com>
2024-07-14 21:47:21 +0800

    feat(unocss.config.ts): add unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -5,4 +5,14 @@ export default defineConfig({
     presetUno(),
     presetAttributify(),
   ],
+  shortcuts: {
+    'head-0': 'text-[25px] font-bold',
+    'head-1': 'text-[22px] font-bold',
+    'head-2': 'text-[18px] font-bold',
+    'head-3': 'text-[16px] font-bold lh-[20px]',
+    'text-default': 'text-[13px] lh-[16px]',
+    'text-paragraph': 'text-[13px] lh-[19px]',
+    'text-medium': 'text-[12px] lh-[15px]',
+    'text-small': 'text-[11px] lh-[14px]',
+  },
 })
