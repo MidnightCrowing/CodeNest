6f3e7569f59291e214839424f4802a9f7a7c651d
MidnightCrowing <110297461+MidnightCrowing@users.noreply.github.com>
2024-12-03 19:50:49 +0800

    fix: resolve CI error in workflow file

--- jetv-ui/scripts/copy-file.js
+++ jetv-ui/scripts/copy-file.js
@@ -1,8 +1,8 @@
 import fs from 'node:fs'
 import path from 'node:path'
 
-const sourcePath = path.join(__dirname, 'src', 'assets', 'jet-icons.json')
-const destPath = path.join(__dirname, 'jet-icons.json')
+const sourcePath = path.resolve('src', 'assets', 'jet-icons.json')
+const destPath = path.resolve('jet-icons.json')
 
 fs.copyFileSync(sourcePath, destPath)
 console.log('File copied successfully')
