5f38cf4790963bf7ba675e6e6e1bc14d852e5576
MidnightCrowing <110297461+MidnightCrowing@users.noreply.github.com>
2024-12-09 20:27:30 +0800

    fix: modify ProjectOverview to support temporary projects

--- src/views/Home/ProjectOverview/ProjectOverview.vue
+++ src/views/Home/ProjectOverview/ProjectOverview.vue
@@ -3,54 +3,51 @@ import { JeFrame, JeLine } from 'jetv-ui'
 import { useI18n } from 'vue-i18n'
 
 import { LicensePop, mainLangPop, ProjectCard } from '~/components/ProjectCard'
-import { ProjectKind } from '~/constants/localProject'
+import type { ProjectKind } from '~/constants/localProject'
 import { projectManager } from '~/core/main'
 
+import type { SidePanelActive } from '../types'
 import ProjectOverviewHeader from './ProjectOverviewHeader.vue'
 
 const props = defineProps<{
-  activatedItem: string
+  activatedItem: SidePanelActive
 }>()
 
 const { t } = useI18n()
 
 const searchValue = ref('')
 
-// 根据激活的菜单项获取 kind (去除前缀 k-)
-const kind = computed(() => {
-  const kindMatch = props.activatedItem.match(/^k-(\w+)$/)
-  return kindMatch ? kindMatch[1] : 'all'
-})
-
-// 根据激活的菜单项获取语言 (去除前缀 l-)
-const lang = computed(() => {
-  const langMatch = props.activatedItem.match(/^l-(.+)$/)
-  return langMatch ? langMatch[1] : ''
-})
-
-// 根据选择的 kind 和 lang 获取项目列表
+// 根据选择的 kind、temp 和 lang 获取项目列表
 const projects = computed(() => {
-  if (kind.value === 'all') {
-    return lang.value ? projectManager.getProjectsByLang(lang.value) : projectManager.getProjects()
-  }
-  else {
-    const projectKindMap: Record<string, ProjectKind> = {
-      mine: ProjectKind.MINE,
-      fork: ProjectKind.FORK,
-      clone: ProjectKind.CLONE,
-      test: ProjectKind.TEST,
-    }
+  const activatedItem = props.activatedItem
 
-    if (lang.value) {
-      // 如果有语言参数，返回指定语言的项目
-      return projectManager.getProjectsByLang(lang.value).filter(project =>
-        projectManager.getProjectsByKind(projectKindMap[kind.value]).includes(project),
-      )
+  // 匹配以 k- 开头的字符串
+  if (activatedItem.startsWith('k-')) {
+    const content = activatedItem.slice(2) // 去掉前面的 "k-"
+
+    if (content === 'all') {
+      return projectManager.getProjects()
     }
     else {
-      // 没有语言参数，按 kind 获取项目
-      return projectManager.getProjectsByKind(projectKindMap[kind.value])
+      return projectManager.getProjectsByKind(content as ProjectKind)
     }
+  }
+
+  // 匹配 temp 字符串
+  else if (activatedItem === 'temp') {
+    return projectManager.getTempProjects()
+  }
+
+  // 匹配以 l- 开头的字符串
+  else if (activatedItem.startsWith('l-')) {
+    const content = activatedItem.slice(2) // 去掉前面的 "l-"
+
+    // 如果有语言参数，返回指定语言的项目
+    return projectManager.getProjectsByLang(content)
+  }
+
+  else {
+    throw new Error(`Invalid activatedItem: ${props.activatedItem}`)
   }
 })
 
