a7a228508e56665d4a3fec0ac3af1eb424f5bc15
MidnightCrowing <110297461+MidnightCrowing@users.noreply.github.com>
2025-01-12 11:50:15 +0800

    fix: set dataPath in userData

--- electron/utils/dataPath.ts
+++ electron/utils/dataPath.ts
@@ -1,8 +1,8 @@
-import { homedir } from 'node:os'
 import { resolve } from 'node:path'
 
-const userHome = homedir()
-export const dataPath = resolve(userHome, '.codenest')
+import { app } from 'electron'
+
+export const dataPath = app.getPath('userData')
 
 export const dataFilePath = resolve(dataPath, 'projects.json')
 export const settingsFilePath = resolve(dataPath, 'settings.json')
